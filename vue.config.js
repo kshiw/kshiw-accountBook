e03d221065d8dd39362c6cec9db6f2884d5b308f
tel_13632031302 <asd123456>
2019-06-28 00:59:16 +0800

    edit2

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,16 @@
 module.exports = {
-  configureWebpack: config => {
-  	require('@vux/loader').merge(config, {
-  	  	plugins: ['vux-ui', {
-  	  		name: 'less-theme',
-  	  		path: 'src/theme.less'
-  	  	}]
-  	})
-  }
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/kshiw-accountBook/'
+        : '/',
+    outputDir: 'docs',
+    // eslint-loader 是否在保存的时候检查
+    lintOnSave: true,
+    configureWebpack: config => {
+        require('@vux/loader').merge(config, {
+            plugins: ['vux-ui', {
+                name: 'less-theme',
+                path: 'src/theme.less'
+            }]
+        })
+    }
 }
