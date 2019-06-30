7fc76aaa10c0aa41ca8912d656b0a83951bd0750
tel_13632031302 <asd123456>
2019-06-30 16:12:54 +0800

    w

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const Timestamp = new Date().getTime();
+
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
         ? '/kshiw-accountBook/'
@@ -5,6 +7,11 @@ module.exports = {
     outputDir: 'docs',
     // eslint-loader 是否在保存的时候检查
     lintOnSave: true,
+    chainWebpack: config => {
+        // 输出重构  打包编译后的 文件名称  【模块名称.版本号.时间戳】
+        config.output.filename(`name].[hash].${Timestamp}.js`)
+        config.output.chunkFilename(`[name].[hash].${Timestamp}.js`)
+    },
     configureWebpack: config => {
         require('@vux/loader').merge(config, {
             plugins: ['vux-ui', {
